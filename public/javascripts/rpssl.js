efd5511b746bf16460cb0aea49af5d1fc08b762c
Zaephor <github@draconrose.com>
2015-03-22 23:59:12 -0700

    Added client-side JS, finished serverside logic, cleaned up misc code, moved css and js files to be hosted via express.static as per assignment step 4

---
+++ public/javascripts/rpssl.js
@@ -0,0 +1,79 @@
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
+var main = function () {
+    "use strict";
+
+    var setPlayer = function (options) {
+        $("#player").append(
+            $("<ul/>")
+        );
+        $.each(options, function (key, val) {
+            $("#player ul").append(
+                $("<li/>").addClass(val).append(
+                    $("<a/>", {href: "#"}).addClass("play").append(val)
+                )
+            );
+        });
+    };
+    var setComputer = function (options) {
+        $("#computer").append(
+            $("<ul/>")
+        );
+        $.each(options, function (key, val) {
+            $("#computer ul").append(
+                $("<li/>").addClass(val).append(val)
+            );
+        });
+    };
+
+    var setScore = function() {
+      $("#score").append(
+          $("<div/>").addClass("progress").text("Will load when you take your first turn.")
+      );
+    };
+
+    var updateScore = function (data) {
+        var stats = {sum: data.score.wins + data.score.losses + data.score.ties};
+        stats.win = Math.floor((data.score.wins / stats.sum) * 100).toString() + "%";
+        stats.tie = Math.floor((data.score.ties / stats.sum) * 100).toString() + "%";
+        stats.loss = Math.floor((data.score.losses / stats.sum) * 100).toString() + "%";
+        $("#score div").replaceWith(
+            $("<div/>").addClass("progress").append(
+                $("<div/>").addClass("progress-bar").addClass("progress-bar-success").css("width", stats.win).append(
+                    $("<span/>").text(stats.win + "(" + data.score.wins + ")")
+                )
+            ).append(
+                $("<div/>").addClass("progress-bar").addClass("progress-bar-warning").css("width", stats.tie).append(
+                    $("<span/>").text(stats.tie + "(" + data.score.ties + ")")
+                )
+            ).append(
+                $("<div/>").addClass("progress-bar").addClass("progress-bar-danger").css("width", stats.loss).append(
+                    $("<span/>").text(stats.loss + "(" + data.score.losses + ")")
+                )
+            )
+        );
+        $("#score h2").replaceWith(
+            $("<h2/>").text("Score - "+data.last.outcome)
+        );
+    };
+
+    $.getJSON("/play/options", function (data) {
+        setPlayer(data);
+        setComputer(data);
+        setScore(); // Dummy data
+
+        // Dom should be fully configured by this stage
+        $("a.play").click(function (event) {
+            event.preventDefault();
+            var playerChoice = event.target.innerText;
+            $.getJSON("/play/" + playerChoice, function (data) {
+                $("#player ul li").removeClass("played");
+                $("#computer ul li").removeClass("played");
+                $("#player ul li." + data.last.user).addClass("played");
+                $("#computer ul li." + data.last.computer).addClass("played");
+                updateScore(data)
+            });
+        });
+    });
+
+};
+$(document).ready(main);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
-var express = require('express');
+/* jshint node: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
+"use strict";
+
+var express = require("express");
 var router = express.Router();
 
+var rpssl = {
+    last: { outcome: "none", user: "none", computer: "none" },
+    score: { "wins": 0, "losses": 0, "ties": 0 },
+    playOptions:  {
+        "rock": ["lizard", "scissors"],
+        "paper": ["rock", "spock"],
+        "scissors": ["paper", "lizard"],
+        "lizard": ["paper", "spock"],
+        "spock": ["rock", "scissors"]
+    },
+    playTurn: function(userPlayed){
+        var randomNumber = Math.floor(Math.random() * Object.keys(this.playOptions).length);
+        this.last.computer = Object.keys(this.playOptions)[randomNumber];
+        this.last.user = userPlayed;
+
+        if (this.last.user === this.last.computer) {
+            this.score.ties++;
+            this.last.outcome = "tie";
+        } else if (this.playOptions[this.last.user].indexOf(this.last.computer) > -1) {
+            this.score.wins++;
+            this.last.outcome = "win";
+        } else if (this.playOptions[this.last.computer].indexOf(this.last.user) > -1) {
+            this.score.losses++;
+            this.last.outcome = "lose";
+        } else {
+            return "failed?";
+        }
+    }
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", {title: "Rock Paper Scissors Lizard Spock"});
+});
+
+/* Handle post /play requests */
+router.all("/play/:action", function (req, res, next) {
+    if(typeof rpssl.playOptions[req.params.action.toLowerCase()] !== "undefined"){
+        // Play user's turn
+        rpssl.playTurn(req.params.action.toLowerCase());
+        res.json({score: rpssl.score, last: rpssl.last});
+    } else if(req.params.action.toLowerCase() === "options") {
+        // Return options
+        res.json(Object.keys(rpssl.playOptions));
+    } else {
+        // Return error
+        res.json({error:"Invalid Request"});
+    }
+
 });
 
 module.exports = router;
